2f8e838dbd45b064b927d01b5654e44899bcb4ee
Jack Martin <jrm9229@gmail.com>
2025-09-09 15:09:59 -0500

    husky and lint-staged test 3

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "archipelacode" is now active!');
-  conssole.log("i am testing lint-staged");
+  console.log("i am testing lint-staged");
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
