89da7a6dc895546c46699ce7e90400007468d5c5
Jack Martin <jrm9229@gmail.com>
2025-09-09 15:47:21 -0500

    setup some more stuff

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,36 @@
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-export default [{
+export default [
+  {
     files: ["**/*.ts"],
-}, {
+  },
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      "@typescript-eslint": typescriptEslint,
     },
 
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: "module",
     },
 
     rules: {
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
+      "@typescript-eslint/naming-convention": [
+        "warn",
+        {
+          selector: "import",
+          format: ["camelCase", "PascalCase"],
+        },
+      ],
 
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
+      curly: "warn",
+      eqeqeq: "warn",
+      "no-throw-literal": "warn",
+      semi: "warn",
     },
-}];
+  },
+  eslintPluginPrettierRecommended,
+];
--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,40 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import * as archipelago from "./archipelago";
+import * as leetcode from "./leetcode";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "archipelacode" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  const disposable = vscode.commands.registerCommand(
-    "archipelacode.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from ArchipelaCode!");
-    },
+  context.subscriptions.push(
+    vscode.commands.registerCommand("archipelacode.start", async () => {
+      let apHostname = await vscode.window.showInputBox({
+        placeHolder: "Hostname of your AP room (Ex. 'archipelago.gg')",
+        prompt: "Please enter the hostname of your Archipelago room",
+        value: "archipelago.gg",
+      });
+      apHostname ? apHostname : "archipelago.gg";
+      let apPort = await vscode.window.showInputBox({
+        placeHolder: "Port of your AP room (Ex. '42315')",
+        prompt: "Please enter the port of your Archipelago room",
+      });
+      apPort ? Number(apPort) : 0;
+      let apSlotname = await vscode.window.showInputBox({
+        placeHolder: "Username in your AP room (Ex. 'ShackledMars')",
+        prompt: "Please enter your username shown in your Archipelago room",
+      });
+      apSlotname ? apSlotname : "";
+      let apPassword = await vscode.window.showInputBox({
+        placeHolder: "Password for your AP room (leave blank if no password)",
+        prompt: "Please enter the password for your Archipelago room",
+      });
+      apPassword ? apPassword : "";
+      vscode.window.showInformationMessage(`https://${apHostname}:${apPort}`);
+      vscode.window.showInformationMessage(`${apSlotname}`);
+      vscode.window.showInformationMessage(`${apPassword}`);
+    }),
   );
-
-  context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
